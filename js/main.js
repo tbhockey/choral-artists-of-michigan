152755a07d2cceb7e34d02bf1ddf4f82dc07f147
Tony Beninate <tonybeninate@icloud.com>
2019-09-17 20:54:28 -0400

    Basic page content and nav. #2

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function remoteContent(type) {
   if(type == 'nav') {
     var content = "<nav class='navbar navbar-expand-lg navbar-light bg-light'>" +
       "<div class='container'>" +
-        "<a class='navbar-brand' href='#'>" +
+        "<a class='navbar-brand' href='index.html'>" +
           "<img src='images/logo.png' height='60' alt='Choral Artists of Michigan'>" +
         "</a>" +
         "<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>" +
@@ -18,19 +18,19 @@ function remoteContent(type) {
         "<div class='collapse navbar-collapse' id='navbarNav'>" +
           "<ul class='navbar-nav mr-auto'>" +
             "<li class='nav-item'>" +
-              "<a class='nav-link' id='nav-link-index' href='#'>Home</a>" +
+              "<a class='nav-link' id='nav-link-index' href='index.html'>Home</a>" +
             "</li>" +
             "<li class='nav-item'>" +
               "<a class='nav-link' href='#'>Upcoming</a>" +
             "</li>" +
             "<li class='nav-item'>" +
-              "<a class='nav-link' href='#'>History</a>" +
+              "<a class='nav-link' id='nav-link-history' href='history.html'>History</a>" +
             "</li>" +
             "<li class='nav-item'>" +
-              "<a class='nav-link' href='#'>Bios</a>" +
+              "<a class='nav-link' id='nav-link-bios' href='bios.html'>Bios</a>" +
             "</li>" +
             "<li class='nav-item'>" +
-              "<a class='nav-link' href='#'>Contact</a>" +
+              "<a class='nav-link' id='nav-link-contact' href='contact.html'>Contact</a>" +
             "</li>" +
           "</ul>" +
           "<ul class='navbar-nav'>" +
